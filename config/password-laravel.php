ef37ef6418fdc9223ee44689d987b38681485d69
Anthony Porthouse <anthony.porthouse@groundsix.com>
2017-04-27 14:54:26 +0100

    Use separated rules and apply as needed

--- config/password-laravel.php
+++ config/password-laravel.php
@@ -2,13 +2,61 @@
 
 return [
     /**
-     * Password config.
+     * The password blacklist file path.
+     * If left null, the default provided one will be used. This is the SecLists Top 10000 passwords list
+     * (https://github.com/danielmiessler/SecLists/blob/master/Passwords/10_million_password_list_top_10000.txt)
      *
-     * By default we apply some sensible default validators that follow NIST best practices
+     * Default: null
      */
-    'config' => [
-        new \Groundsix\Password\Validators\PasswordLengthValidator(),
-        new \Groundsix\Password\Validators\PasswordDomainValidator(config('app.domain')),
-        new \Groundsix\Password\Validators\PasswordBlacklistValidator(),
-    ],
+    'blacklist_file' => null,
+
+    /**
+     * The domain of the application.
+     * Used to filter out uses of the domain as the password.
+     *
+     * Default: ''
+     */
+    'domain' => '',
+
+    /**
+     * Minimum password length.
+     * Bigger is better. A minimum value of 8 is suggested by the NIST.
+     *
+     * Default: 8
+     */
+    'minimum_length' => 8,
+
+    /**
+     * Minimum number of numeric characters in the password.
+     * Not usually necessary if a decent blacklist is used alongside a reasonable minimum length.
+     *
+     * Default: 1
+     */
+    'minimum_numeric' => 1,
+
+    /**
+     * Special character rules
+     * Not usually necessary if a decent blacklist is used alongside a minimum length.
+     */
+    'special' => [
+
+        /**
+         * Minimum number of special characters to require
+         *
+         * Default: 1
+         */
+        'minimum' => 1,
+
+        /**
+         * Whitelist of what is considered a special character.
+         * The default provided uses the OWASP list of special characters.
+         * (https://www.owasp.org/index.php/Password_special_characters)
+         *
+         * Default: [...]
+         */
+        'whitelist' => [
+            ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<',
+            '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',
+        ],
+    ]
 ];
--- src/PasswordServiceProvider.php
+++ src/PasswordServiceProvider.php
@@ -5,7 +5,10 @@ namespace Groundsix\PasswordLaravel;
 use Groundsix\Password\PasswordException;
 use Groundsix\Password\PasswordValidator;
 use Groundsix\Password\Validators\PasswordBlacklistValidator;
+use Groundsix\Password\Validators\PasswordDomainValidator;
 use Groundsix\Password\Validators\PasswordLengthValidator;
+use Groundsix\Password\Validators\PasswordMinimumNumberValidator;
+use Groundsix\Password\Validators\PasswordMixedCaseValidator;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,8 +21,56 @@ class PasswordServiceProvider extends ServiceProvider
             __DIR__.'/../config/password-laravel.php' => $this->app->configPath().'/'.'password-laravel.php',
         ], 'config');
 
-        Validator::extend('password', function ($attribute, $value, $parameters, $validator) {
-            $passwordValidator = new PasswordValidator(config('password-laravel.config'));
+        Validator::extend('password_blacklist', function ($attribute, $value) {
+            $passwordValidator = new PasswordBlacklistValidator(config('password-laravel.blacklist_file'));
+            try {
+                return $passwordValidator->validate($value);
+            } catch (PasswordException $e) {
+                return false;
+            }
+        });
+
+        Validator::extend('password_domain', function ($attribute, $value) {
+            $passwordValidator = new PasswordDomainValidator(config('password-laravel.domain'));
+            try {
+                return $passwordValidator->validate($value);
+            } catch (PasswordException $e) {
+                return false;
+            }
+        });
+
+        Validator::extend('password_minimum_length', function ($attribute, $value) {
+            $passwordValidator = new PasswordLengthValidator(config('password-laravel.minimum_length'));
+            try {
+                return $passwordValidator->validate($value);
+            } catch (PasswordException $e) {
+                return false;
+            }
+        });
+
+        Validator::extend('password_mixed_case', function ($attribute, $value) {
+            $passwordValidator = new PasswordMixedCaseValidator();
+            try {
+                return $passwordValidator->validate($value);
+            } catch (PasswordException $e) {
+                return false;
+            }
+        });
+
+        Validator::extend('password_minimum_numeric', function ($attribute, $value) {
+            $passwordValidator = new PasswordMinimumNumberValidator(config('password-laravel.minimum_numeric'));
+            try {
+                return $passwordValidator->validate($value);
+            } catch (PasswordException $e) {
+                return false;
+            }
+        });
+
+        Validator::extend('password_minimum_special', function ($attribute, $value) {
+            $passwordValidator = new PasswordMinimumNumberValidator(
+                config('password-laravel.special.minimum'),
+                config('password-laravel.special.whitelist')
+            );
             try {
                 return $passwordValidator->validate($value);
             } catch (PasswordException $e) {
